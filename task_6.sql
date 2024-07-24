0f5efdf68015f1dbfb3b2a065aad1f49dd692509
mathooko <mutuasteven254@gmail.com>
2024-07-24 18:04:46 +0300

    typo in 124 Happiness  Ave.

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,3 @@
-INSERT INTO customer VALUES(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.' );
+INSERT INTO customer VALUES(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.' );
 INSERT INTO customer VALUES(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave' );
 INSERT INTO customer VALUES(4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave' );
