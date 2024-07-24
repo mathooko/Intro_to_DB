a76afb319f615bf38ef6931ba65db56cf48eb5b8
mathooko <mutuasteven254@gmail.com>
2024-07-24 14:22:20 +0300

    schema definition of books table

---
+++ alx_book_store.sql
@@ -0,0 +1,6 @@
+CREATE TABLE BOOKS(book _id number (10) Constraint books_book_id_pk PRIMARY KEY,
+                    title varchar (130),
+                    author_id NUMBER(10),
+                    price DOUBLE(9,2),
+                    publication_date DATE
+                    )
